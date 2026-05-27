164757eaac7db4bc556127ae1b3f36af9fc14019
Franco Daniel Luvisotti Junco <121893433+FrancoLuvisotti@users.noreply.github.com>
2026-05-27 23:12:59 +0000

    inciando

---
+++ js/main.js
@@ -0,0 +1,37 @@
+// js/main.js
+
+const themeToggle = document.getElementById("theme-toggle");
+
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+  document.documentElement.setAttribute(
+      "data-theme",
+          currentTheme
+            );
+
+              themeToggle.textContent =
+                  currentTheme === "dark" ? "🌙" : "☀️";
+                  }
+
+                  themeToggle.addEventListener("click", () => {
+
+                    let theme =
+                        document.documentElement.getAttribute("data-theme");
+
+                          if (theme === "dark") {
+                              theme = "light";
+                                  themeToggle.textContent = "☀️";
+                                    } else {
+                                        theme = "dark";
+                                            themeToggle.textContent = "🌙";
+                                              }
+
+                                                document.documentElement.setAttribute(
+                                                    "data-theme",
+                                                        theme
+                                                          );
+
+                                                            localStorage.setItem("theme", theme);
+
+                                                            });
