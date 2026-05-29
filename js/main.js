9cd098fe6114befff11eebbb015667380b334cb4
FrancoLuvisotti <franco_luvi@hotmail.com>
2026-05-28 23:36:47 -0300

    identacion

--- js/main.js
+++ js/main.js
@@ -5,33 +5,23 @@ const themeToggle = document.getElementById("theme-toggle");
 const currentTheme = localStorage.getItem("theme");
 
 if (currentTheme) {
-  document.documentElement.setAttribute(
-      "data-theme",
-          currentTheme
-            );
+  document.documentElement.setAttribute("data-theme", currentTheme);
 
-              themeToggle.textContent =
-                  currentTheme === "dark" ? "🌙" : "☀️";
-                  }
+  themeToggle.textContent = currentTheme === "dark" ? "🌙" : "☀️";
+}
 
-                  themeToggle.addEventListener("click", () => {
+themeToggle.addEventListener("click", () => {
+  let theme = document.documentElement.getAttribute("data-theme");
 
-                    let theme =
-                        document.documentElement.getAttribute("data-theme");
+  if (theme === "dark") {
+    theme = "light";
+    themeToggle.textContent = "☀️";
+  } else {
+    theme = "dark";
+    themeToggle.textContent = "🌙";
+  }
 
-                          if (theme === "dark") {
-                              theme = "light";
-                                  themeToggle.textContent = "☀️";
-                                    } else {
-                                        theme = "dark";
-                                            themeToggle.textContent = "🌙";
-                                              }
+  document.documentElement.setAttribute("data-theme", theme);
 
-                                                document.documentElement.setAttribute(
-                                                    "data-theme",
-                                                        theme
-                                                          );
-
-                                                            localStorage.setItem("theme", theme);
-
-                                                            });
+  localStorage.setItem("theme", theme);
+});
